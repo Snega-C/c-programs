087b31cd38b3f612015cb17db45b67e8e14d79aa
Snega C <141730767+Snega-C@users.noreply.github.com>
2024-01-22 20:07:41 +0530

    convert Fahrenheit degrees to Centgrade degree.c

--- Fahrenheit degrees to Centgrade degree.c
+++ Fahrenheit degrees to Centgrade degree.c
@@ -7,5 +7,7 @@ int main()
     cd=(fd-32)*(5.0/9.0);
     printf("\nThe Centgrade degree is %0.5f ",cd);
     return 0;
-
+  /* output
+      Enter a Fahrenheit degree:100
+      The Centgrade degree is 37.77778*/
 }
