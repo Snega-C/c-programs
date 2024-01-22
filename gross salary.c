6299ad3be9e2f1e4eb3c7620a2be527362eec578
Snega C <141730767+Snega-C@users.noreply.github.com>
2024-01-22 20:01:52 +0530

    Update gross salary.c
    Get a basic salary from input to calculate gross salary with 40% dearness allowance and 20% house rent.

--- gross salary.c
+++ gross salary.c
@@ -11,4 +11,9 @@ int main()
     gross_salary=basic_salary+dearness+house_rent;
     printf("\nThe gross salary=%0.2f\n",gross_salary);
     return 0;
+    /*output
+        Enter a basic salary:40000
+        The dearness is:16000.00
+        The house rent is:8000.00
+        The gross salary=64000.00*/
 }
